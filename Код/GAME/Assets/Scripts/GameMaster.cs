ce833f12a5db2d2f20889497feff0a5ce10ff0e7
AndeyGordeev <33543084+AndeyGordeev@users.noreply.github.com>
2019-09-15 16:41:41 +0300

    Добавлено меню завершения игры
    пока что срабатывает только при падении, ибо хп у героя еще нет

--- Код/GAME/Assets/Scripts/GameMaster.cs
+++ Код/GAME/Assets/Scripts/GameMaster.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
-    
+
+    public static GameMaster gm;
+    public GameObject gameOverUI;
+
     public static void KillPlayer(Play player)
     {
         Destroy(player);
+        gm.EndGame();
+    }
+
+    private void Start()
+    {
+        if (gm == null)
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    private void EndGame()
+    {
+        gameOverUI.SetActive(true);
     }
 
     public static void KillEnemy(Enemy enemy)
--- Код/GAME/Assets/Scripts/PauseMenu.cs
+++ Код/GAME/Assets/Scripts/PauseMenu.cs
@@ -8,20 +8,24 @@ public class PauseMenu : MonoBehaviour
 
     public static bool gameIsPaused = false;
 
+    public GameObject gameOverUI;
     public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (!gameOverUI.activeSelf)
         {
-            if (gameIsPaused)
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                Resume();
-            }
-            else
-            {
-                Pause();
+                if (gameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
             }
         }
     }
