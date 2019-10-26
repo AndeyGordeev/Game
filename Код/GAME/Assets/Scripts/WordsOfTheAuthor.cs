1031e0495958c0077238476990a07fd2f92c7a8c
AndeyGordeev <33543084+AndeyGordeev@users.noreply.github.com>
2019-10-26 19:54:28 +0300

    Сделана краткая версия первой сцены

---
+++ Код/GAME/Assets/Scripts/WordsOfTheAuthor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WordsOfTheAuthor : MonoBehaviour
+{
+    public TextMeshProUGUI textDisp;
+    [TextArea(3, 10)]
+    public string[] sentences;
+
+    public float[] pointDialog;
+    
+    private int numberWords;
+
+    public GameObject nextButt;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pointDialog = new[] {-14f,-4f,15f,24f};//точки на оси ОХ, где включается монолог
+    }
+
+    private void FixedUpdate()
+    {
+        if (pointDialog.Length > numberWords)
+        {
+            if (player.transform.position.x >= pointDialog[numberWords] - 0.5 && player.transform.position.x <= pointDialog[numberWords] + 0.5)
+            {
+                textDisp.text = sentences[numberWords];
+                Time.timeScale = 0f;
+                nextButt.SetActive(true);
+                numberWords++;
+            }
+        }
+
+        if (Input.anyKeyDown && nextButt.activeSelf)
+        {
+            NextSentence();
+        }
+    }
+
+    public void NextSentence()
+    {
+        textDisp.text = "";
+        nextButt.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
