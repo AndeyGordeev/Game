f4a260986c5b50e98c8a18bc0b99f9e0ccd4ea54
Helen <helencheremiskina@gmail.com>
2019-10-27 16:23:35 +0300

    Изменение обложки

--- Код/GAME/Assets/Scripts/MainMenu.cs
+++ Код/GAME/Assets/Scripts/MainMenu.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public Sprite Back2;
     public void PlayGame()
     {
+        GameObject.Find("Background").GetComponent<Image>().sprite = Back2;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
