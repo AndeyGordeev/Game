40622175a2f8e6ab9b834d716bbbf5c97680abc3
Helen <helencheremiskina@gmail.com>
2019-11-09 10:31:43 +0300

    Изменение обложки

--- Код/GAME/Assets/Scripts/MainMenu.cs
+++ Код/GAME/Assets/Scripts/MainMenu.cs
@@ -6,10 +6,18 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public Sprite Back2;
     public void PlayGame()
     {
-        GameObject.Find("Background").GetComponent<Image>().sprite = Back2;
+        StartCoroutine(Example());
+    }
+
+    public IEnumerator Example()
+    {
+        for (int i = 10; i > -1; i--)
+        {
+            GameObject.Find("Background").GetComponent<Image>().color = new Color(1f, 1f, 1f, i * 0.1f);
+            yield return new WaitForSeconds(.3f);
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -18,5 +26,4 @@ public class MainMenu : MonoBehaviour
         Debug.Log("QUIT!");
         Application.Quit();
     }
-
 }
